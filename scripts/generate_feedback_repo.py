9e9089cacf3736db8c97603182fdea750c7cc172
pankajpansari <pankaj.pansari1@gmail.com>
2025-05-29 06:58:21 +0000

    Feedback for repo works fine

--- scripts/generate_feedback_repo.py
+++ scripts/generate_feedback_repo.py
@@ -1,19 +1,20 @@
-import sys
 import subprocess
+from pathlib import Path
+from unidiff import PatchSet
+import os
+import sys
 from openai import OpenAI
 from pydantic import BaseModel, Field
-from pathlib import Path
 import json
 import textwrap
 from dotenv import load_dotenv
-import os
 import datetime
 import argparse
 
-# ===================== LOAD CONFIG ================================
+THRESHOLD = 10
 
 # Load config.env file
-load_dotenv(dotenv_path="../config.env")
+load_dotenv(dotenv_path="config_repo.env")
 
 # File paths 
 PROBLEM_STATEMENT = os.getenv('PROBLEM_STATEMENT')
@@ -24,8 +25,6 @@ INTER_DIR = os.getenv('INTER_DIR')
 
 # LLM Models
 PROPOSER_REVIEWER = os.getenv('PROPOSER_REVIEWER')
-SUMMARIZER = os.getenv('SUMMARIZER')
-
 
 # ===================== UTILS ====================================
 
@@ -98,23 +97,18 @@ class Annotation(BaseModel):
   comment: str = Field(description="Detailed feedback about code at this line number")
   severity: str = Field(description="Level of importance: 'suggestion', 'issue', or 'critical'")
 
-class Summary(BaseModel):
-  strengths: str = Field(description="Description of positive aspects of the submission") 
-  areas_for_improvement: str = Field(description="Description of aspects that need improvement")
-  overall_assessment: str = Field(description="Brief overall evaluation of the submission")
-
 class FeedbackResponse(BaseModel):
   annotations: list[Annotation] = Field(description="List of line-specific code feedback")
-  summary: Summary = Field(description="Overall assessment of the submission")
-  
-# ======================================================================
 
+# ======================================================================
 client = OpenAI()
+
 
 # Proposer generates a first draft of annotations 
 def call_proposer(problem_statement, rubric, submission_program, input_filename):
 
-  prompt = f"""See the following problem statement of OS assignment, the rubric for code quality feedback, and one C program submission.
+  prompt = f"""For an OS assignment on xv6 OS, the student has made modifications to the xv6 repo in repose to following problem statement. Here is one file from the repo that has been modified with the added lines prepended by + symbol. Note that this is only a part of the solution of the assignment. 
+
   <problem_statement>
   {problem_statement}
   </problem_statement>
@@ -127,7 +121,7 @@ def call_proposer(problem_statement, rubric, submission_program, input_filename)
   {rubric}
   </rubric>
   
-  Suggest a list of annotations (comments) of feedback based on the rubric. Also give a summary. Adhere to the structured output schema.
+  Suggest a list of annotations (comments) of feedback based on the rubric. Adhere to the structured output schema. Give feedback on the modified lines. It's okay to not give any feedback if there's is not a strong need for one.
   """
 
   try:
@@ -146,7 +140,7 @@ def call_proposer(problem_statement, rubric, submission_program, input_filename)
     return f"API call error for proposer: {str(api_error)}"
 
   initial_feedback = proposer_response.output_parsed
-  
+
   json_file = Path(INTER_DIR) / input_filename.relative_to(INPUT_DIR).parent / f"{input_filename.stem}_intermediate.json"
 
   with open(json_file, 'w') as f:
@@ -167,8 +161,8 @@ def call_reviewer(problem_statement, rubric, submission_program, input_filename)
     
   proposal_json = json.dumps(proposer_output_data)
 
-  linter_summary = run_linter(input_filename)  
-  prompt = f"""See the following problem statement of OS assignment, the rubric for code quality feedback, and one C program submission.
+  prompt = f"""For an OS assignment on xv6 OS, the student has made modifications to the xv6 repo in repose to following problem statement. Here is one file from the repo that has been modified with the added lines prepended by + symbol. Note that this is only a part of the solution of the assignment. 
+
   <problem_statement>
   {problem_statement}
   </problem_statement>
@@ -180,17 +174,10 @@ def call_reviewer(problem_statement, rubric, submission_program, input_filename)
   <rubric>
   {rubric}
   </rubric>
-  
-  Clang-tidy linter gave the following output (summary) for the submission.
-  
-  <linter>
-  {linter_summary}
-  </linter>
-  
+ 
   Look at the following list of annotations and the summary of feedback. Do the following:
   1. For each annotation, check if line number is correct and if annotation is useful to give and valid
-  2. Incorporate the linter output in annotations and summary, if needed.
-  3. Discard annotations which are not very helpful and may clutter.
+  2. Discard annotations which are not very helpful and may clutter.
   
   <feedback>
   {proposal_json}
@@ -200,7 +187,7 @@ def call_reviewer(problem_statement, rubric, submission_program, input_filename)
     reviewer_response = client.responses.parse(
       model = PROPOSER_REVIEWER,
       input=[
-        {"role": "system", "content": "Your role is to act as an OS course TA who provides qualitative feedback on student C programming assignment. Feedback is good when it is relevant for education of undergraduate computer science students, and it is not overwhelming in quantity. Please stick to the rubric."},
+        {"role": "system", "content": "Your role is to act as an OS course TA who provides qualitative feedback on student C programming assignment. Feedback is good when it is relevant for education of undergraduate computer science students, and it is not overwhelming in quantity. Please stick to the rubric. Specifically, your role is to act as a reviewer of feedback comments proposed by a Proposer LLM."},
         {
           "role": "user",
           "content": prompt,
@@ -219,30 +206,19 @@ def call_reviewer(problem_statement, rubric, submission_program, input_filename)
     json.dump(refined_feedback.model_dump(), f, indent = 4, ensure_ascii=False)
 
   write_log(reviewer_response, "Reviewer", input_filename)
- 
-# This function inserts the feedback comments at the correct point in original code and appends a summary at the end
+
+
 def postprocess(input_filename):
 
   json_file = Path(INTER_DIR) / input_filename.relative_to(INPUT_DIR).parent / f"{input_filename.stem}_final.json"
 
   f = open(json_file, 'r')
   x = json.load(f)
-  summary = json.dumps(x['summary'])
   
-  try:
-    response = client.responses.create(
-      model = SUMMARIZER,
-      input = [
-          {"role": "user", "content": "The following is summary of feedback on a C program from an automated tool. First, summarize it nicely so I can append it at the bottom of submission. Then format it properly as a C comment block; try to respect 80 character line limit convention. Do not add any suggestions of your own; give the comment block output so I can insert it as it is.\n<summary>\n" + summary + "\n</summary>"}
-      ]
-    )
-  except Exception as api_error:
-    return f"API call error for proposer: {str(api_error)}"
+  # If there are no annotations, skip writing to output file
+  if len(x['annotations']) == 0:
+     return
 
-  write_log(response, "Summarizer", input_filename)
-  
-  summary = response.output_text
-  
   annotation_dict = {}
   for annotation in x['annotations']:
       line_number = annotation['line_number']
@@ -253,28 +229,23 @@ def postprocess(input_filename):
   
   f_input = open(input_filename, 'r')
   
-  output_filename = Path(OUTPUT_DIR) / input_filename.parent.relative_to('input') / Path(input_filename.stem + '_feedback' + input_filename.suffix)
+  output_filename = Path(OUTPUT_DIR) / input_filename.parent.relative_to(INPUT_DIR) / Path('feedback.c')
 
-  f_output = open(output_filename, 'w')
+  f_output = open(output_filename, 'a')
   i = 0
+  f_output.write(f'\n/*============================{input_filename.name}=========================================/*\n')
   for line in f_input:
       i += 1
       if i in annotation_dict.keys():
           comment = annotation_dict[i]
           comment = "\n" + comment + "\n"
           f_output.write(comment)
-      f_output.write(line) 
+          f_output.write(f"line {i}: {line[2:]}") 
       
-  f_output.write("\n" + summary + "\n")
   f_input.close()
   f_output.close()
 
-def main():
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument("input_program_filepath", help = "Path of C program to be evaluated")
-  args = parser.parse_args()
-  input_filename = Path(args.input_program_filepath)
+def generate_file_feedback(input_filename):
 
   try:
     with open(PROBLEM_STATEMENT, 'r') as f:
@@ -288,11 +259,9 @@ def main():
   except FileNotFoundError:
     print(f"Error: {RUBRIC} not found")
   
-  # Create intermediate directories in ouput/ and intermediates/ if needed
-  output_path = Path(OUTPUT_DIR) / input_filename.parent.relative_to('input')
-  intermediates_path = Path(INTER_DIR) / input_filename.parent.relative_to('input')
+  # Create intermediate directories in output/ and intermediates/ if needed
+  output_path = Path(OUTPUT_DIR) / input_filename.parent.relative_to(INTER_DIR)
   os.makedirs(output_path, exist_ok = True)
-  os.makedirs(intermediates_path, exist_ok = True)
 
   submission_program = preprocess_input(input_filename) 
   call_proposer(problem_statement, rubric, submission_program, input_filename)
@@ -302,6 +271,73 @@ def main():
 
   print(f"Feedback generation complete for {input_filename}. Output saved.")
 
+def main():
+
+    print(INTER_DIR)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source_repo_path", help = "Path of original repo (source)")
+    parser.add_argument("target_repo_path", help = "Path of modified repo (target)")
+    args = parser.parse_args()
+    source_repo = Path(args.source_repo_path)
+    target_repo = Path(args.target_repo_path)
+
+    process = subprocess.Popen(['diff', '-r', '-u', source_repo, target_repo], stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+    diff_out, diff_err = process.communicate()
+    os.makedirs(Path(INTER_DIR) / target_repo.parent.relative_to('input'), exist_ok=True)
+    diff_filename = Path(INTER_DIR) / target_repo.relative_to('input') / 'repo.diff'
+    with open(diff_filename, 'w') as f:
+        f.write(diff_out)
+
+    """
+    # Generate feedback for new files (files only in target_repo) 
+    target_str = 'Only in ' + str(target_repo)
+    with open(diff_filename, 'r') as f:
+      for line in f:
+        if target_str in line:
+          filename = line.split()[-1]
+          if filename.endswith('.c'):
+            input_filename = target_repo / filename
+            # output here means processed version of input program file; prepend + to all added lines in program file
+            # in this case, all lines are prepended with +
+            output_filename = Path(INTER_DIR) / input_filename.parent.relative_to('input') / f"{input_filename.name}"
+            f_input = open(input_filename, 'r')
+            f_output = open(output_filename, 'w')
+            for line in f_input:
+              f_output.write('+ ' + line)
+
+            f_input.close()
+            f_output.close() 
+
+            generate_file_feedback(output_filename)
+    """
+      # Generate feedback for modified files
+    patch = PatchSet.from_filename(diff_filename)
+    for p in patch:
+        if Path(p.source_file).suffix == '.c':
+            input_filename = Path(p.target_file)
+            hunk_set = set() 
+
+            for hunk in p:
+                for i in range(hunk.target_length):
+                    hunk_set.add(hunk.target_start + i)
+
+            if len(hunk_set) < THRESHOLD:
+                continue
+            f_input = open(input_filename, 'r')
+            output_filename = Path(INTER_DIR) / input_filename.parent.relative_to('input') / f"{input_filename.name}"
+
+            f_output = open(output_filename, 'w')
+            i = 0
+            for line in f_input:
+                i += 1
+                if i in hunk_set: 
+                    f_output.write('+ ' + line)
+                else:
+                    f_output.write(line) 
+            
+            f_input.close()
+            f_output.close()
+            generate_file_feedback(output_filename)
 
 if __name__ == "__main__":
-  main()
+   main()
